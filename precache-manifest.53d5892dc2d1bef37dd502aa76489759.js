39c3a44c19a5af0ea3244625be4e98c34216c023
Alyssa Karina <60025410+yawneazy@users.noreply.github.com>
2023-02-18 21:43:06 -0700

    Updates

---
+++ precache-manifest.53d5892dc2d1bef37dd502aa76489759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e30fd4801f7a2421065",
+    "url": "%7Bakyanez%7D.github.io/%7Byaneza%7D/static/css/main.ddc7521e.chunk.css"
+  },
+  {
+    "revision": "6e30fd4801f7a2421065",
+    "url": "%7Bakyanez%7D.github.io/%7Byaneza%7D/static/js/main.8de1796a.chunk.js"
+  },
+  {
+    "revision": "540c35ecffb268c42cc6",
+    "url": "%7Bakyanez%7D.github.io/%7Byaneza%7D/static/js/runtime~main.5547746d.js"
+  },
+  {
+    "revision": "3427c80b127e01e9bbb2",
+    "url": "%7Bakyanez%7D.github.io/%7Byaneza%7D/static/js/2.65912059.chunk.js"
+  },
+  {
+    "revision": "7b3fe5851c44d00723f4cb6e0c020cf3",
+    "url": "%7Bakyanez%7D.github.io/%7Byaneza%7D/static/media/office.7b3fe585.jpg"
+  },
+  {
+    "revision": "303ee79080472f2ae0875ef5524ee600",
+    "url": "%7Bakyanez%7D.github.io/%7Byaneza%7D/index.html"
+  }
+];
