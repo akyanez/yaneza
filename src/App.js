7d116226b9f2e1507045f717def912c2817a2da1
Alyssa Karina <60025410+yawneazy@users.noreply.github.com>
2023-02-18 17:37:26 -0700

    aerg`:wq

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About'
-// import NavbarList from './Components/Navbar/Navbar';
+import NavbarList from './Components/Navbar/Navbar';
 
 function App() {
   return (
@@ -23,24 +23,7 @@ function App() {
      </> 
     
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Digital Black Religion Timeline
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Digital Black Religion Timeline
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+
 }
 
 export default App;
